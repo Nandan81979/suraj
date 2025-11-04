7a6d7b5e780c76cb3e6a6fdca3cd6b90788ba827
Nandan81979 <nandanshettar8197@gmail.com>
2025-11-04 11:34:22 +0530

    Create userdetails.py

---
+++ userdetails.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv) == 3:
+script name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+print("User provided input values:")
+else:
+script name = sys.argv[0]
+name = "nandan"
+rollno = "028"
+print("No input given using default values:")
+print("Script Name:", script name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
+Devops-Pythonman040
+Spaces 4
